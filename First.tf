bc61d5566e477925b03c03f5d2588bda86f32eae
Prem <dubeyprem690@gmail.com>
2024-02-16 01:13:38 +0530

    dsdasdad

--- First.tf
+++ First.tf
@@ -63,6 +63,10 @@ resource "aws_route_table_association" "public" {
 # Create route table for private subnet
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
   tags = {
 Name = "Private_RT"
 }
@@ -71,7 +75,6 @@ Name = "Private_RT"
 # Associate private subnet with private route table
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
-  gateway_id = aws_nat_gateway.nat.id
   route_table_id = aws_route_table.private.id
 }
 
