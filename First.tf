b42595e42c223bf0fdba85c4a3cdddc82d0ea234
Prem <dubeyprem690@gmail.com>
2024-02-16 00:07:29 +0530

    new

--- First.tf
+++ First.tf
@@ -63,11 +63,7 @@ resource "aws_route_table" "public" {
 # Create route table for private subnet
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+  
   tags = {
     Name = "Private_RT"
   }
