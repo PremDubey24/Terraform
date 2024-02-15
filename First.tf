e88d64780a16b724a125499fb7fcdf687ff07e4e
Prem <dubeyprem690@gmail.com>
2024-02-15 21:11:23 +0530

    new

--- First.tf
+++ First.tf
@@ -42,7 +42,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 # Attach internet gateway to VPC
-resource "aws_vpc_attachment" "gw_attachment" {
+resource "aws_internet_gateway_attachment" "gw_attachment" {
   vpc_id              = aws_vpc.main.id
   internet_gateway_id = aws_internet_gateway.gw.id
 }
@@ -57,6 +57,19 @@ resource "aws_route_table" "public" {
   }
   tags = {
     Name = "Public_RT"
+  }
+}
+
+# Create route table for private subnet
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Private_RT"
   }
 }
 
